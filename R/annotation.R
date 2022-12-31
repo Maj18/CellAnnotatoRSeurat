1ce85c072da2403b7339e1dd2216319e74b8e0ea
YUAN LI <45690154+Maj18@users.noreply.github.com>
2022-12-31 07:52:41 +0100

    Update annotation.R
    Add #' @export

--- R/annotation.R
+++ R/annotation.R
@@ -9,6 +9,7 @@ usethis::use_package('ggplot2')
 #' @param obj a seurat object
 #' @param clr2change classification to be relabeled
 #' @param new.cluster.ids new label, make sure the order in new.cluster.ids should correspond to that in levels(obj@meta.data[, clr2change])
+#' @export
 changeLabel <- function(obj, clr2change, new.cluster.ids) {
   Idents(obj) <- obj@meta.data[, clr2change]
   names(new.cluster.ids) <- levels(obj@meta.data[, clr2change])
@@ -22,6 +23,7 @@ changeLabel <- function(obj, clr2change, new.cluster.ids) {
 #' @param obj a seurat object
 #' @param marker.list a list of markers for different cell types
 #' @param classification classification to be annotated
+#' @export
 annOnMarker <- function(obj, marker.list, classification="SCT_snn_res.0.3") {
   idf.wei <- log(1+ncol(obj)/(rowSums(as.matrix(obj@assays$SCT@data>0)) + 1))
   tf.idf <- Matrix(t(as.matrix(obj@assays$SCT@data)), sparse=TRUE)
@@ -69,6 +71,7 @@ annOnMarker <- function(obj, marker.list, classification="SCT_snn_res.0.3") {
 #' @param marker.list a list of markers for different cell types
 #' @param classification classification to be annotated
 #' @param plot.margin  plot margin
+#' @export
 stackedVlnPlot <- function(obj, marker.list, plot.margin = unit(c(0.9, 1, 1, 2), "cm")) {
   Idents(obj) <- obj$annotation_cluster
   markers <- marker.list[levels(obj$annotation_cluster)] %>% unlist
